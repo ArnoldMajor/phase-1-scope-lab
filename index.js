42602443e6d02204ce56eed0f89e2f4a3ddeb734
ArnoldMajor <arnoldmwangakala@gmail.com>
2024-03-24 04:02:32 +0300

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName () {
+    return customerName = 'BOB';
+}
+
+function setBestCustomer(){
+    return bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+    return bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'Frank';
+
+function changeLeastFavoriteCustomer(){
+    return leastFavoriteCustomer = 'Jane';
+}
+
+console.log (changeLeastFavoriteCustomer())
